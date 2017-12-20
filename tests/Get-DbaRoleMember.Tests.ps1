8d10dee55fabb1529d0b55c0899e4956fa0db06c
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 09:04:02 +0000

    added test file for Get-DbaRoleMember

---
+++ tests/Get-DbaRoleMember.Tests.ps1
@@ -0,0 +1,13 @@
+﻿$CommandName = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1","")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$CommandName Unit Tests" -Tag UnitTests, Get-DbaRoleMember {
+    Context "$Command Name Input" {
+
+    }
+}
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+
+}
