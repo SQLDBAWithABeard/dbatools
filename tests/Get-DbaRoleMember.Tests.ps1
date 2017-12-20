611f1d8833a63aac24b2e7e731e2b52aa5e6d0f3
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 09:08:44 +0000

    added tests for parameters

--- tests/Get-DbaRoleMember.Tests.ps1
+++ tests/Get-DbaRoleMember.Tests.ps1
@@ -4,7 +4,34 @@ Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 Describe "$CommandName Unit Tests" -Tag UnitTests, Get-DbaRoleMember {
     Context "$Command Name Input" {
-
+        $Params = (Get-Command Get-DbaRoleMember).Parameters
+        It "Should have a mandatory parameter SQLInstance" {
+            $Params['SQLInstance'].Attributes.Mandatory | Should be $true
+        }
+        It "Should have Alias of ServerInstance and SqlServer for Parameter SQLInstance"{
+            $params['SQLInstance'].Aliases | Should Be @('ServerInstance','SqlServer')
+        }
+        It "Should have a parameter SqlCredential" {
+            $Params['SqlCredential'].Count | Should Be 1
+        }
+        It "Should have Alias of Credential for Parameter SQLCredential"{
+            $params['SQLCredential'].Aliases | Should Be @('Credential')
+        }
+        It "Should have a parameter Database" {
+            $Params['Database'].Count | Should Be 1
+        }
+        It "Should have Alias of Databases for Parameter Database"{
+            $params['Database'].Aliases | Should Be @('Databases')
+        }
+        It "Should have a parameter ExcludeDatabase" {
+            $Params['ExcludeDatabase'].Count | Should Be 1
+        }
+        It "Should have a parameter IncludeServerLevel" {
+            $Params['IncludeServerLevel'].Count | Should Be 1
+        }
+        It "Should have a parameter NoFixedRole" {
+            $Params['NoFixedRole'].Count | Should Be 1
+        }
     }
 }
 
